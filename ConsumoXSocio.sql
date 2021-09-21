d74ebf4b9b273c4027eea60e4c950c02e87907d5
tapiaht <hiver.tapia@uab.edu.bo>
2021-09-20 22:31:18 -0400

    moviendo a carpeta db

--- ConsumoXSocio.sql
+++
@@ -1,6 +0,0 @@
-SELECT        Consumos.IdConsumo AS c, Periodos.IdPeriodo AS p, Socios.ApellidosNombres AS s, Periodos.FechaInicio, Periodos.FechaFinal, Periodos.Tarifa, Consumos.Cancelado
-FROM            Consumos INNER JOIN
-                         Periodos ON Consumos.IdPeriodo = Periodos.IdPeriodo INNER JOIN
-                         Socios ON Consumos.Cuenta = Socios.Cuenta
-WHERE        (Consumos.Cancelado = 0) AND (Socios.Cuenta = 400)
-ORDER BY s
--- DeudasXsocio.sql
+++
@@ -1,5 +0,0 @@
-SELECT        sum(Periodos.Tarifa)
-FROM            Consumos INNER JOIN
-                         Periodos ON Consumos.IdPeriodo = Periodos.IdPeriodo INNER JOIN
-                         Socios ON Consumos.Cuenta = Socios.Cuenta
-WHERE        (Consumos.Cancelado = 0) AND (Socios.Cuenta = 400)
